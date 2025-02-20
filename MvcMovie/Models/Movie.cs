36189b5721e9155eb6b00c8c9edd9d786601a647
ST10441754 <ST10441754@vcconnect.edu.za>
2025-02-20 16:57:28 +0200

    Third Commit - Step 8 - Almost Complete, last step with package manager console stating Build Failed when trying to Add-Migration Rating.

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,4 +1,4 @@
-﻿using System; 
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -13,6 +13,8 @@ public class Movie
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
+
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+    public string? Rating { get; set; }
 }
